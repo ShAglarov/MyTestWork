36c1aef8e56dfe45d10c8ca8b316f375ca2c2823
Aglarov <shamil.aglarov@gmail.com>
2023-11-09 02:02:13 +0300

    Wrote a server request, handled errors, and retrieved the response status.

--- MyTestApp/Resources/Services/NetworkSwagger.swift
+++ MyTestApp/Resources/Services/NetworkSwagger.swift
@@ -9,23 +9,57 @@ import Foundation
 
 final class NetworkSwagger {
     
+    private let getCodeUrl = "https://dev-hospital.doct24.com/api/v1/auth/login/get/code/"
     private let baseHospitalURL = "https://dev-hospital.doct24.com/api/v1/auth/profile/"
     private let decoder = JSONDecoder()
-
+    
+    func postRequestForCode(phoneNumber: String) async throws -> [String: Any] {
+        guard let url = createURL(from: getCodeUrl) else { throw HError.invalidURL }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let json: [String: Any] = [
+            "phone_number": phoneNumber
+        ]
+        
+        let jsonData = try JSONSerialization.data(withJSONObject: json, options: [])
+        request.httpBody = jsonData
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw HError.invalidResponse
+        }
+        
+        switch httpResponse.statusCode {
+        case 200...299:
+            if let jsonResult = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                return jsonResult
+            } else {
+                throw HError.invalidResponse
+            }
+        default:
+            let errorData = String(data: data, encoding: .utf8) ?? "No error information"
+            throw HError.serverError(code: httpResponse.statusCode, message: errorData)
+        }
+    }
+    
     func getUserProfile(token: String) async throws -> UserProfile {
         return try await postRequest(responseModel: UserProfile.self, token: token)
     }
-
+    
     private func buildURLComponents(from baseURL: String) -> URLComponents? {
         guard let base = URL(string: baseURL), let baseScheme = base.scheme, let baseHost = base.host else {
             print("Invalid base URL")
             return nil
         }
-
+        
         var urlComponents = URLComponents()
         urlComponents.scheme = baseScheme
         urlComponents.host = baseHost
-
+        
         // Определяем, какой части пути следует избежать. Это должен быть общий базовый URL до `/api/`.
         let basePathToAvoid = "\(baseScheme)://\(baseHost)"
         // Убеждаемся, что у нас есть путь, начинающийся с '/api/'.
@@ -36,17 +70,17 @@ final class NetworkSwagger {
             print("Base URL does not contain the path starting with '/api/'")
             return nil
         }
-
+        
         return urlComponents
     }
-
-    private func createFromHospitalURL() -> URL? {
-        var urlComponents = buildURLComponents(from: baseHospitalURL)
+    
+    private func createURL(from baseURL: String) -> URL? {
+        var urlComponents = buildURLComponents(from: baseURL)
         return urlComponents?.url
     }
-
+    
     private func postRequest<T: Codable>(responseModel: T.Type, token: String) async throws -> T {
-        guard let url = createFromHospitalURL() else {
+        guard let url = createURL(from: baseHospitalURL) else {
             throw HError.invalidURL
         }
         var request = URLRequest(url: url)
@@ -54,13 +88,13 @@ final class NetworkSwagger {
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-
+        
         let (data, response) = try await URLSession.shared.data(for: request)
-
+        
         guard let httpResponse = response as? HTTPURLResponse else {
             throw HError.invalidResponse
         }
-
+        
         switch httpResponse.statusCode {
         case 200...299:
             return try decoder.decode(responseModel, from: data)
